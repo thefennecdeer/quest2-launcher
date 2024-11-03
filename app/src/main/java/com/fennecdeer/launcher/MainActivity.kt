bfeec5085a9f32fc3e194325b9ee5597d7a983e5
Troy Takac <thefennecdeer@gmail.com>
2024-11-04 09:24:27 +1100

    Fix secrets

--- app/src/main/java/com/fennecdeer/launcher/MainActivity.kt
+++ app/src/main/java/com/fennecdeer/launcher/MainActivity.kt
@@ -1,58 +1,25 @@
 package com.fennecdeer.launcher
 
 import android.app.Activity
-import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_IMMUTABLE
-import android.app.admin.DevicePolicyManager
-import android.app.admin.SystemUpdatePolicy
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.content.IntentSender
-import android.os.BatteryManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.UserManager
-import android.provider.Settings
 import android.util.Log
-import android.view.View
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.fennecdeer.launcher.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import se.vidstige.jadb.JadbConnection
-import se.vidstige.jadb.JadbDevice
-import se.vidstige.jadb.JadbException
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.util.Timer
-import kotlin.concurrent.timerTask
 
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var ADBManager: ADBManager
 
     private val KIOSK_PACKAGE = "com.wearecypha.vrroadtrip"
 
-
     private val TAG = MainActivity::class.java.simpleName
-
-
-
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,13 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         //mDevicePolicyManager.removeActiveAdmin(mAdminComponentName)
         Log.i(TAG,"aaa!")
-
-
-        CoroutineScope(Dispatchers.IO).launch {
-            ADBManager = ADBManager()
-            val adbDevice = ADBManager.initADB()
-            ADBManager.pauseGuardian(adbDevice)
-        }
 
 
 //        Timer().schedule(timerTask {
